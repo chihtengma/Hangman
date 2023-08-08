f91ff9927a1302e959c68ee1d48bb6c2a2ae5c91
ChihTeng Ma <chihtengma416@gmail.com>
2023-08-08 12:16:07 -0400

    Init

---
+++ src/App.tsx
@@ -0,0 +1,93 @@
+import { useCallback, useEffect, useState } from "react";
+import words from "./wordList.json";
+import { HangmanDrawing } from "./HangmanDrawing";
+import { HangmanWord } from "./HangmanWord";
+import { Keyboard } from "./keyboard";
+
+function getWord() {
+   return words[Math.floor(Math.random() * words.length)];
+}
+
+function App() {
+   const [wordToGuess, setWordToGuess] = useState(getWord);
+   const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+   const incorrectLetters = guessedLetters.filter((letter) => !wordToGuess.includes(letter));
+
+   const isLoser = incorrectLetters.length >= 6;
+   const isWinner = wordToGuess.split("").every((letter) => guessedLetters.includes(letter));
+
+   const addGuessedLetter = useCallback(
+      (letter: string) => {
+         if (guessedLetters.includes(letter) || isLoser || isWinner) return;
+
+         setGuessedLetters((currentLetters) => [...currentLetters, letter]);
+      },
+      [guessedLetters, isWinner, isLoser]
+   );
+
+   useEffect(() => {
+      const handler = (e: KeyboardEvent) => {
+         const key = e.key;
+
+         if (!key.match(/^[a-z]$/)) return;
+
+         e.preventDefault();
+         addGuessedLetter(key);
+      };
+
+      document.addEventListener("keypress", handler);
+
+      return () => {
+         document.removeEventListener("keypress", handler);
+      };
+   }, [guessedLetters]);
+
+   useEffect(() => {
+      const handler = (e: KeyboardEvent) => {
+         const key = e.key;
+
+         if (key !== "Enter") return;
+
+         e.preventDefault();
+         setGuessedLetters([]);
+         setWordToGuess(getWord());
+      };
+
+      document.addEventListener("keypress", handler);
+
+      return () => {
+         document.removeEventListener("keypress", handler);
+      };
+   }, [guessedLetters]);
+
+   return (
+      <div
+         style={{
+            maxWidth: "800px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+            margin: "0 auto",
+            alignItems: "center"
+         }}
+         className="vh-100 mt-5">
+         <div style={{ fontSize: "2rem", textAlign: "center" }}>
+            <h1 className="display-1 fw-bold mb-4">Hangman</h1>
+            {isWinner && "Winner! - Refresh to try again!"} {isLoser && "Nice Try - Refresh to try again!"}
+         </div>
+         <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+         <HangmanWord guessedLetters={guessedLetters} wordToGuess={wordToGuess} reveal={isLoser} />
+         <div style={{ alignSelf: "stretch" }}>
+            <Keyboard
+               activeLetters={guessedLetters.filter((letter) => wordToGuess.includes(letter))}
+               inActiveLetters={incorrectLetters}
+               addGuessedLetter={addGuessedLetter}
+               disabled={isWinner || isLoser}
+            />
+         </div>
+      </div>
+   );
+}
+
+export default App;
---
+++ src/Keyboard.tsx
@@ -0,0 +1,57 @@
+import styles from "./Keyboard.module.css";
+
+const KEYS = [
+   "a",
+   "b",
+   "c",
+   "d",
+   "e",
+   "f",
+   "g",
+   "h",
+   "i",
+   "j",
+   "k",
+   "l",
+   "m",
+   "n",
+   "o",
+   "p",
+   "q",
+   "r",
+   "s",
+   "t",
+   "u",
+   "v",
+   "w",
+   "x",
+   "y",
+   "z"
+];
+
+type KeyboardProps = {
+   activeLetters: string[];
+   inActiveLetters: string[];
+   addGuessedLetter: (letter: string) => void;
+   disabled?: boolean;
+};
+
+export function Keyboard({ activeLetters, inActiveLetters, addGuessedLetter, disabled }: KeyboardProps) {
+   return (
+      <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))", gap: ".5rem" }}>
+         {KEYS.map((key) => {
+            const isActive = activeLetters.includes(key);
+            const isInactive = inActiveLetters.includes(key);
+            return (
+               <button
+                  onClick={() => addGuessedLetter(key)}
+                  className={`${styles.btn} ${isActive ? styles.active : ""} ${isInactive ? styles.inactive : ""}`}
+                  disabled={isInactive || isActive || disabled}
+                  key={key}>
+                  {key}
+               </button>
+            );
+         })}
+      </div>
+   );
+}
